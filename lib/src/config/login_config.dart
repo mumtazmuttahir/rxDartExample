81f91ae2fe91bc89e635ce75d1c1ad36f9498d87
Code Runner <mumtazmuttahir@users.noreply.github.com>
2021-12-14 05:58:07 +0100

    add validation text for email and password

--- lib/src/config/login_config.dart
+++ lib/src/config/login_config.dart
@@ -2,8 +2,10 @@ class LoginConfiguration {
   static const LOGIN_CONTAINER_ALL_MARGIN = 20.0;
   static const EMAIL_TEXT_FIELD_LABEL_TEXT = 'Email Address';
   static const EMAIL_TEXT_FIELD_HINT_TEXT = 'xyz@example.com';
+  static const EMAIL_TEXT_FIELD_VALIDATOR_TEXT = 'Please enter a valid email';
   static const PASSWORD_TEXT_FIELD_LABEL_TEXT = 'Password';
   static const PASSWORD_TEXT_FIELD_HINT_TEXT = 'Enter Password';
+  static const PASSWORD_TEXT_FIELD_VALIDATOR_TEXT = 'Please enter password greater than 4 characters';
   static const PASSWORD_TEXT_FIELD_MARGIN_BOTTOM = 20.0;
   static const SUBMIT_BUTTON_TEXT = 'Submit';
 }
