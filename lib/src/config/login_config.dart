84ae604f0579945124e576fe3458224f110a47f8
Code Runner <mumtazmuttahir@users.noreply.github.com>
2021-12-14 05:18:43 +0100

    add the magic numbers required in a the login screen

---
+++ lib/src/config/login_config.dart
@@ -0,0 +1,9 @@
+class LoginConfiguration {
+  static const LOGIN_CONTAINER_ALL_MARGIN = 20.0;
+  static const EMAIL_TEXT_FIELD_LABEL_TEXT = 'Email Address';
+  static const EMAIL_TEXT_FIELD_HINT_TEXT = 'xyz@example.com';
+  static const PASSWORD_TEXT_FIELD_LABEL_TEXT = 'Password';
+  static const PASSWORD_TEXT_FIELD_HINT_TEXT = 'Enter Password';
+  static const PASSWORD_TEXT_FIELD_MARGIN_BOTTOM = 20.0;
+  static const SUBMIT_BUTTON_TEXT = 'Submit';
+}
