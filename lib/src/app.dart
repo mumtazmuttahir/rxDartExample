99f24b15f086728fce8303bccf7bc2c926168255
Code Runner <mumtazmuttahir@users.noreply.github.com>
2021-12-14 04:07:11 +0100

    create a new entry class

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: "RxDart Login App",
+      home: Scaffold(
+        body: Text('Show a form here!'),
+      ),
+    );
+  }
+}
