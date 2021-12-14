45eb686d91d6d4d1fbfe77a2ca3f8188da9afe7e
Code Runner <mumtazmuttahir@users.noreply.github.com>
2021-12-14 06:27:16 +0100

    add mixin strings

--- lib/src/config/login_config.dart
+++ lib/src/config/login_config.dart
@@ -3,9 +3,11 @@ class LoginConfiguration {
   static const EMAIL_TEXT_FIELD_LABEL_TEXT = 'Email Address';
   static const EMAIL_TEXT_FIELD_HINT_TEXT = 'xyz@example.com';
   static const EMAIL_TEXT_FIELD_VALIDATOR_TEXT = 'Please enter a valid email';
+  static const EMAIL_TEXT_FIELD_NULL_VALIDATOR_TEXT = 'Value is null';
   static const PASSWORD_TEXT_FIELD_LABEL_TEXT = 'Password';
   static const PASSWORD_TEXT_FIELD_HINT_TEXT = 'Enter Password';
   static const PASSWORD_TEXT_FIELD_VALIDATOR_TEXT = 'Please enter password greater than 4 characters';
+  static const PASSWORD_TEXT_FIELD_NULL_VALIDATOR_TEXT = 'Value is null';
   static const PASSWORD_TEXT_FIELD_MARGIN_BOTTOM = 20.0;
   static const SUBMIT_BUTTON_TEXT = 'Submit';
 }
